974cc7409f3a2eaf88b65b9365ca83cadb91a0c8
Phatthapol <phatthapol.sak@gmail.com>
2026-02-26 13:37:24 +0700

    Add server.js package config

---
+++ server.js
@@ -0,0 +1,94 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
+const connectDB = require('./config/db');
+
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const {xss} = require('express-xss-sanitizer');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
+
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+//Load env vars
+dotenv.config({ path: './config/config.env' });
+
+//Connect to database
+connectDB();
+
+//routes files
+const companies = require('./routes/companies');
+const auth = require('./routes/auth');
+const registrations  = require('./routes/registrations');
+
+const app = express();
+
+// Use extended query parser
+app.set('query parser', 'extended'); 
+
+//Body parser
+app.use(express.json());
+
+//Cookie parser
+app.use(cookieParser());
+
+//Sanitize data
+app.use(mongoSanitize());
+
+//Helmet
+app.use(helmet());
+
+//Prevent XSS attacks
+app.use(xss());
+
+//Rate limiting
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000, //10 mins
+    max: 100
+});
+app.use(limiter);
+
+//Prevent http param pollution
+app.use(hpp());
+
+//Enable CORS
+app.use(cors());
+
+const swaggerOptions = {
+    swaggerDefinition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Online Job Fair Registration API',
+            version: '1.0.0',
+            description: 'A simple Online Job Fair Registration API'
+        },
+        servers: [
+            {
+                url: 'http://localhost:5000/api/v1',
+            }
+        ]
+    },
+    apis: ['./routes/*.js']
+};
+
+const swaggerDocs = swaggerJsdoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+
+//Mount routers
+app.use('/api/v1/companies', companies);
+app.use('/api/v1/auth', auth);
+app.use('/api/v1/registrations', registrations);
+
+const PORT = process.env.PORT || 5000;
+const server = app.listen(PORT, console.log('Server running in', process.env.NODE_ENV, 'mode on port', PORT));
+
+//Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    //Close server & exit process
+    server.close(() => process.exit(1));
+});
