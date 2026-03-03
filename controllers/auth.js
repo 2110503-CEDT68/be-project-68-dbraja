6ef4570332d2350b3b9d860af5b634c82589bdce
Kongpob7k <kongpopteen@gmail.com>
2026-03-03 09:35:43 +0700

    remove authorize in auth controllers

--- controllers/auth.js
+++ controllers/auth.js
@@ -55,16 +55,6 @@ exports.getMe=async(req,res,next)=>{
     });
 };
 
-exports.authorize = (...roles)=>{
-    return (req,res,next)=>{
-        if(!roles.includes(req.user.role)){
-            return res.status(403).json({
-                success:false,message:`User role ${req.user.role} is not authorized to access this route`
-            });
-        }
-        next();
-    }
-}
 
 exports.logout = async (req, res) => {
   res.cookie('token', 'none', {
